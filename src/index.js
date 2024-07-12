2a53c80962fc2e4177c22fa49010b8a1eabdc97c
Juniper Watson <lookitsartemis@gmail.com>
2024-07-12 01:18:32 -0500

    Added some stuff :3

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const { Client, IntentsBitField } = require("discord.js");
+require("dotenv").config();
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+client.on("ready", (c) => {
+  console.log(`✅ Bot logged in as ${c.user.tag} [${c.user.id}].`);
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) {
+    return;
+  }
+
+  if (message.content === "hello") {
+    message.reply("hello");
+  }
+});
+
+client.login(process.env.TOKEN);
